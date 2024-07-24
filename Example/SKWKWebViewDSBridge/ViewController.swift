3893269dac3193761fd1832f32c186b266fdb53e
shuaike <718401263@qq.com>
2024-07-24 11:25:04 +0800

    feat:提交代码

--- Example/SKWKWebViewDSBridge/ViewController.swift
+++ Example/SKWKWebViewDSBridge/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import SKWKWebViewDSBridge
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       let classMethods = getClassMethods(of: MyClass.self)
+       print(classMethods)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +24,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+class MyClass {
+    class func classMethod1() {}
+    class func classMethod2() {}
+}
+ 
+func getClassMethods(of classType: AnyClass) -> [String] {
+    var classMethods = [String]()
+    let mirror = Mirror(reflecting: classType)
+    
+    if let children = mirror.children.filter({ $0.value is Selector }) as? [Mirror.Child]{
+        for child in children {
+            if let methodName = child.label {
+                classMethods.append(methodName)
+            }
+        }
+    }
+    
+    return classMethods
 }
 
